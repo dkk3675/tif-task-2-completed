9bd376312181bc3d5ddc9389eb06f09de8c21ecf
Divya <karan.boss1000@gmail.com>
2023-02-12 15:09:59 +0530

    initial commit

--- src/components/formComponents/FormExport.tsx
+++ src/components/formComponents/FormExport.tsx
@@ -3,4 +3,6 @@ import dynamic from 'next/dynamic';
 
 const FormExport = dynamic(() => import('./FormSelect'), { ssr: false })
 
+FormExport.displayName = 'FormExport';
+
 export default (props:IFormSelectProps) => <FormExport name={props.name} label={props.label} placeholder={props.placeholder} value={props.value} onChange={props.onChange} onBlur={props.onBlur} error={props.error} touched={props.touched} selectProps={props.selectProps} children={props.children} helperText={props.helperText} wrapperProps={props.wrapperProps} options={props.options} />
